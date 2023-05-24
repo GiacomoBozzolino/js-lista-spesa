035217c89f3fe3fd58d363e60867a7d07a71150a
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-24 15:56:11 +0200

    [FEAT] impostazione ciclo while

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let list = [
+    'Pomodoro',
+    'Farina',
+    'Lievito',
+    'Basilico',
+    'Mozzarella',
+    'Olio',
+    'Salame piccante',
+    'Acqua'
+    ]
+
+    console.log(list)
+
+    let shopping = document.getElementById('lista')
+
+    
+    let i = 0
+    
+
+    while (i < list.length){
+        let item = list[i];
+        i++;
+        console.log(item)
+    };
+
+
+    
